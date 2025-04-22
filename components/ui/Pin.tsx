69e238ed02346abe15e8b4c384dc1b9bc6046818
advika31 <advikasinghal31@gmail.com>
2025-04-22 17:04:39 +0530

    `Refactor RecentProjects and Pin components to add links and improve accessibility`

--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -68,12 +68,13 @@ export const PinPerspective = ({
 }) => {
   return (
     // change w-96 to w-full
-    <motion.div className="pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
+    <motion.div className="w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
           <a
             href={href}
-            target={"_blank"}
+            target="_blank"
+            rel="noopener noreferrer"
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
@@ -89,7 +90,7 @@ export const PinPerspective = ({
             perspective: "1000px",
             transform: "rotateX(70deg) translateZ(0)",
           }}
-          className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
+          className="pointer-events-none absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
         >
           <>
             <motion.div
