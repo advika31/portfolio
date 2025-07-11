04d51dce1540ae4c14b202d08347158cca4fa4de
Advika Singhal <145430314+advika31@users.noreply.github.com>
2025-07-12 04:35:28 +0530

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
